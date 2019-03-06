e5adfd25f4e01e699268916cfb67e29326b355fb
peter_alfie <alfie1104@gmail.com>
2019-03-06 19:47:59 +0900

    initial setting is done

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { AppLoading, Font } from "expo";
+import { Ionicons } from "@expo/vector-icons";
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    loaded: false
+  }
+
+  loadAssets = async () => {
+    await Font.loadAsync({
+      ...Ionicons.font
+    });
+  }
+
+  handleError = (error) => console.log(error);
+
+  handleLoaded = () => this.setState({ loaded: true });
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    const { loaded } = this.state;
+
+    if (loaded) {
+      return (
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      );
+    } else {
+      return <AppLoading startAsync={this.loadAssets} onFinish={this.handleLoaded} onError={this.handleError} />;
+    }
   }
 }
 
